29bba6fa8083b57eb198b0f698a114bd2b012b28
KiwiScreams <screamskiwi3@gmail.com>
2024-08-10 21:27:12 +0400

    creating data

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,16 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Create = () => {
-    return ( 
-        <>
-        <h1>Create</h1>
-        <form>
-            <label htmlFor="name">Name:</label>
-            <input type="text" name="name" />
-            <label htmlFor="email">Email:</label>
-            <input type="email" name="email" />
-            <button>submit</button>
-        </form>
-        </>
-     );
-}
- 
+  const [inputData, setInputData] = useState({
+    name: "",
+    email: "",
+  });
+  const navigate = useNavigate();
+  const handleSubmit = (event) =>
+  {
+    event.preventDefault();
+    axios.post('http://localhost:3030/users', inputData).then(res => 
+    {
+        alert('success');
+        navigate("/");
+    }
+    )
+  }
+  return (
+    <>
+      <h1>Create</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => setInputData({ ...inputData, name: e.target.value })}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          onChange={(e) => setInputData({ ...inputData, email: e.target.value })}
+        />
+        <button>submit</button>
+      </form>
+    </>
+  );
+};
+
 export default Create;
