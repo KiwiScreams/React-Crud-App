ed2bc977a0572c744fbb59a67e567af2baf50f8d
KiwiScreams <screamskiwi3@gmail.com>
2024-08-10 22:59:03 +0400

    header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return ( 
+        <>
+        <header>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/create">Create</Link>
+            </nav>
+        </header>
+        </>
+     );
+}
+ 
+export default Header;
