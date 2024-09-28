1b00a5bc9c24b9de4d7191323f090d2577bec85d
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 13:29:55 +0400

    title

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,28 @@ import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
 import Update from "./pages/update/Update";
 import Read from "./pages/read/Read";
-
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        document.title = "Crud App | Home";
+        break;
+      case "/create":
+        document.title = "Crud App | Create";
+        break;
+      case "/update/":
+        document.title = "Crud App | Update";
+        break;
+      case "/read":
+        document.title = "Crud App | Detail";
+        break;
+      default:
+        document.title = "Crud App";
+    }
+  }, [location]);
   return (
     <>
       <Routes>
