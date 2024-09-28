796cb0710d03b0ae599a0efaa850b7001b4c64e4
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 14:14:25 +0400

    not found page

--- src/pages/not found/NotFound.jsx
+++ src/pages/not found/NotFound.jsx
@@ -1,8 +1,18 @@
 import "./NotFound.css";
+import notfoundImage from "../../assets/not-found.png";
 const NotFound = () => {
-    return ( 
-        <>no</>
-     );
-}
- 
+  return (
+    <>
+      <section className="notfound-section">
+        <img src={notfoundImage} alt="" />
+        <h2>Oops!</h2>
+        <p>
+          It looks like the page you're looking for doesn't exist. It might have
+          been moved, deleted, or never existed in the first place.
+        </p>
+      </section>
+    </>
+  );
+};
+
 export default NotFound;
