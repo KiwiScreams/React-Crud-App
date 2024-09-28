7ac3c471464a0092b7d8b2c0fe206a239b1b122b
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 14:21:08 +0400

    navigate to home

--- src/pages/not found/NotFound.jsx
+++ src/pages/not found/NotFound.jsx
@@ -1,6 +1,11 @@
 import "./NotFound.css";
 import notfoundImage from "../../assets/not-found.png";
+import { useNavigate } from "react-router-dom";
 const NotFound = () => {
+  const navigate = useNavigate();
+  const handleNavigateToHomePage = () => {
+    navigate("/");
+  };
   return (
     <>
       <section className="notfound-section">
@@ -10,6 +15,7 @@ const NotFound = () => {
           It looks like the page you're looking for doesn't exist. It might have
           been moved, deleted, or never existed in the first place.
         </p>
+        <button onClick={handleNavigateToHomePage}>Go Home</button>
       </section>
     </>
   );
