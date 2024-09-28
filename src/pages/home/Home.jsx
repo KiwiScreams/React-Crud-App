fc5749dfadddd8059f59fcc9aa78a8f009408a34
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 13:19:27 +0400

    remove search

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,8 +4,6 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import SuccesPanel from "../panel/succes panel/Success";
-import SearchBar from "../search/SearchBar";
-
 const Home = () => {
   const [data, setData] = useState([]);
   const [isSuccess, setIsSuccess] = useState(false);
@@ -16,21 +14,6 @@ const Home = () => {
       .then((res) => setData(res.data))
       .catch((err) => console.log(err));
   }, []);
-
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredData, setFilteredData] = useState([]);
-
-  useEffect(() => {
-    if (searchQuery === "") {
-      setFilteredData(data);
-    } else {
-      const filtered = data.filter((d) =>
-        d.email.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-      setFilteredData(filtered);
-    }
-  }, [data, searchQuery]);
-
   function handleDelete(id) {
     axios.delete("http://localhost:3030/users/" + id).then((res) => {
       setIsSuccess(true);
@@ -39,14 +22,8 @@ const Home = () => {
       }, 2000);
     });
   }
-
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-  };
-
   return (
     <>
-      <SearchBar onChange={(e) => handleSearch(e.target.value)} />
       <section className="home-section">
         <div className="home-container">
           <Link to="/create">
@@ -64,22 +41,13 @@ const Home = () => {
               </tr>
             </thead>
             <tbody>
-              {filteredData.length > 0 ? (
-                filteredData.map((d, i) => {
+              {data.length > 0 ? (
+                data.map((d, i) => {
                   return (
                     <tr key={i}>
                       <td>{d.id}</td>
                       <td>{d.name}</td>
-                      <td
-                        style={{
-                          visibility:
-                            d.email.toLowerCase() === searchQuery.toLowerCase()
-                              ? "visible"
-                              : "hidden",
-                        }}
-                      >
-                        {d.email}
-                      </td>
+                      <td>{d.email}</td>
                       <td className="flex">
                         <Link to={`/update/${d.id}`} className="btn">
                           <i className="fa-solid fa-pen"></i>
@@ -100,7 +68,7 @@ const Home = () => {
               ) : (
                 <tr>
                   <td colSpan="4" className="text-center">
-                    No results found
+                    List is empty
                   </td>
                 </tr>
               )}
